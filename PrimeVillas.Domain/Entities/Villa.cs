9642c5b2c5749884df6291b98e403e5bba111461
Shri Nivas <mrshri404@gmail.com>
2025-03-07 13:04:44 +0530

    basic setup

--- PrimeVillas.Domain/Entities/Villa.cs
+++ PrimeVillas.Domain/Entities/Villa.cs
@@ -11,7 +11,7 @@ namespace PrimeVillas.Domain.Entities
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }    
-        public decimal Price { get; set; }
+        public double Price { get; set; }
         public int  Sqft { get; set; }
         public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
