1d2f45c1c2e25175f1ae14866add7a1e30fad7b5
Shri Nivas <mrshri404@gmail.com>
2025-03-07 12:52:31 +0530

    Add project files.

---
+++ PrimeVillas.Domain/Entities/Villa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeVillas.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }    
+        public decimal Price { get; set; }
+        public int  Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+
+     
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ PrimeVillas.Infrastructure/DATA/VillaContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeVillas.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeVillas.Infrastructure.DATA
+{
+    public class VillaContext:DbContext
+    {
+        public VillaContext(DbContextOptions<VillaContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
