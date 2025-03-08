9130d69f72365090652f5d6cf8e6c6502e6c9109
Shri Nivas <mrshri404@gmail.com>
2025-03-08 10:37:43 +0530

    Add Villa management features and UI enhancements
    - Updated Villa model with validation attributes.
    - Created VillaController for listing and creating villas.
    - Modified layout for dark theme and updated Bootstrap links.
    - Added Create view for new villa entries with validation.
    - Added Index view to display a list of villas with actions.
    - Updated _ViewImports to include necessary namespaces.

---
+++ PrimeVillasWeb/Controllers/VillaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeVillas.Domain.Entities;
+using PrimeVillas.Infrastructure.DATA;
+
+namespace PrimeVillasWeb.Controllers
+{
+    
+    public class VillaController : Controller
+    {
+        private readonly VillaContext _context;
+        public VillaController(VillaContext villaContext)
+        {
+            _context = villaContext;
+        }
+        public IActionResult Index()
+        {
+
+            IEnumerable<Villa> villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villaObj)
+        {
+            if(!ModelState.IsValid)
+            {
+                _context.Villas.Add(villaObj);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(villaObj);
+        }
+    }
+}
