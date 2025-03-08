9e376891354f031a4ace7a7a7eb765b970efc18c
Shri Nivas <mrshri404@gmail.com>
2025-03-08 13:09:04 +0530

    Refactor controllers and update views for villa management
    - Simplified the `Error` action in `HomeController.cs`.
    - Added `Edit` and `Delete` actions in `VillaController.cs` with null checks.
    - Updated `PrimeVillasWeb.csproj` to include a new images folder.
    - Modified `Error.cshtml` to display a not found image.
    - Created new `Delete.cshtml` and `Edit.cshtml` forms with validation.
    - Changed edit action name in `Index.cshtml` from `Update` to `Edit`.
    - Added multiple image files to enhance visual content.

--- PrimeVillasWeb/Controllers/VillaController.cs
+++ PrimeVillasWeb/Controllers/VillaController.cs
@@ -36,5 +36,64 @@ namespace PrimeVillasWeb.Controllers
             }
             return View(villaObj);
         }
+
+        public IActionResult Edit(int villaId)
+        {
+            var obj = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Villa villaObj)
+        {
+
+            if (villaObj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+                _context.Villas.Update(villaObj);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            var obj = _context.Villas.FirstOrDefault(u => u.Id == villaId);
+
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villaObj)
+        {
+            var obj = _context.Villas.FirstOrDefault(u => u.Id == villaObj.Id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _context.Villas.Remove(obj);
+            _context.SaveChanges();
+
+            return RedirectToAction("Error","Home");
+
+        }
+
     }
 }
