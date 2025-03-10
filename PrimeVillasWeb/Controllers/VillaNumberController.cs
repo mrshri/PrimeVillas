b9808fff07a917cdbc0e00d6f4801b65218af02e
Shri Nivas <mrshri404@gmail.com>
2025-03-10 13:20:47 +0530

    Enhance Villa number management with new ViewModel (#4)
    Updated VillaNumberController to use Entity Framework Core and introduced VillaNumberVM for better data handling. Modified actions for Create, Edit, and Delete to improve validation and user feedback. Updated views to utilize the new ViewModel structure, ensuring proper data binding and validation messages. Adjusted notification handling and included the new ViewModel namespace in _ViewImports.cshtml.

--- PrimeVillasWeb/Controllers/VillaNumberController.cs
+++ PrimeVillasWeb/Controllers/VillaNumberController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using PrimeVillas.Domain.Entities;
 using PrimeVillas.Infrastructure.DATA;
+using PrimeVillasWeb.ViewModels;
 
 namespace PrimeVillasWeb.Controllers
 {
@@ -17,37 +19,136 @@ namespace PrimeVillasWeb.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            IEnumerable<VillaNumber> villaNumbers = _context.VillaNumbers.ToList();
+            IEnumerable<VillaNumber> villaNumbers = _context.VillaNumbers.Include(u =>u.Villa).ToList();
             return View(villaNumbers);
         }
 
        
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> list = _context.Villas.ToList().Select(u => new SelectListItem
+            VillaNumberVM villaNumberVM = new()
             {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
 
-            ViewData["villaList"] = list;
-            return View();
+                VillaList = _context.Villas.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+
+            };        
+
+            return View(villaNumberVM);
         }
 
         [HttpPost]
-        public IActionResult Create(VillaNumber villaNumberObj)
+        public IActionResult Create(VillaNumberVM villaNumberObj)
         {
-            if (ModelState.IsValid)
+            Boolean roomNumberExists = _context.VillaNumbers.Any(u => u.Villa_Number == villaNumberObj.VillaNumber.Villa_Number);           
+
+            if (ModelState.IsValid && !roomNumberExists)
             {
-                _context.VillaNumbers.Add(villaNumberObj);
+
+                _context.VillaNumbers.Add(villaNumberObj.VillaNumber);
                 _context.SaveChanges();
                 TempData["success"] = "Villa Number added successfully";
 
                 return RedirectToAction("index");
             }
+
+            if (roomNumberExists)
+            {
+              TempData["error"] = "Villa Number already exists";
+            }
+
+            villaNumberObj.VillaList = _context.Villas.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
             return View(villaNumberObj);
         }
 
+        public IActionResult Edit(int villaNumberId)
+        {
+            VillaNumberVM villaNumberVM = new()
+            {
 
+                VillaList = _context.Villas.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                VillaNumber = _context.VillaNumbers.FirstOrDefault(u => u.Villa_Number == villaNumberId)
+
+            };
+
+            if(villaNumberVM.VillaNumber == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(villaNumberVM);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(VillaNumberVM villaNumberVM)
+        {
+
+            if (ModelState.IsValid )
+            {
+
+                _context.VillaNumbers.Update(villaNumberVM.VillaNumber);
+                _context.SaveChanges();
+                TempData["success"] = "Villa Number Updated successfully";
+
+                return RedirectToAction("index");
+            }
+
+            villaNumberVM.VillaList = _context.Villas.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(villaNumberVM);
+        }
+
+
+        public IActionResult Delete(int villaNumberId)
+        {
+            VillaNumberVM villaNumberVM = new()
+            {
+
+                VillaList = _context.Villas.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                VillaNumber = _context.VillaNumbers.FirstOrDefault(u => u.Villa_Number == villaNumberId)
+
+            };
+
+            if (villaNumberVM.VillaNumber == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(villaNumberVM);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(VillaNumberVM villaNumberVM)
+        {
+            VillaNumber? obj = _context.VillaNumbers
+                .FirstOrDefault(u => u.Villa_Number == villaNumberVM.VillaNumber.Villa_Number);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+
+            }
+            _context.VillaNumbers.Remove(obj);
+            _context.SaveChanges();
+            TempData["success"] = "Villa Number removed successfully.";
+            return RedirectToAction("index");
+        }
     }
 }
