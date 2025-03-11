192a6ed0022257df0d328a786cbf88119a730f57
Shri Nivas <mrshri404@gmail.com>
2025-03-11 12:41:44 +0530

    Implement repository pattern for VillaNumber management
    - Added `Any` method to `IRepository` for entity checks.
    - Updated `IUnitOfWork` to include `VillaNumber` repository.
    - Created `IVillaNumberRepository` with an `Update` method.
    - Implemented `Any` method in `Repository` class.
    - Initialized `VillaNumberRepository` in `UnitOfWork`.
    - Created `VillaNumberRepository` class implementing `IVillaNumberRepository`.
    - Refactored `VillaNumberController` to use `IUnitOfWork` for data access.

--- PrimeVillas.Application/Common/Interfaces/IUnitOfWork.cs
+++ PrimeVillas.Application/Common/Interfaces/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace PrimeVillas.Application.Common.Interfaces
     public interface IUnitOfWork
     {
         IVillaRepository Villa { get; }
+        IVillaNumberRepository VillaNumber { get; }
         void Save();
     }
 }
---
+++ PrimeVillas.Infrastructure/Repository/VillaNumberRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeVillas.Application.Common.Interfaces;
+using PrimeVillas.Domain.Entities;
+using PrimeVillas.Infrastructure.DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeVillas.Infrastructure.Repository
+{
+    public class VillaNumberRepository :Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly VillaContext _context;
+        public VillaNumberRepository(VillaContext villaContext) : base(villaContext)
+        {
+            _context = villaContext;
+        } 
+
+        public void Update(VillaNumber entity)
+        {
+           _context.VillaNumbers.Update(entity);
+        }
+    }
+}
