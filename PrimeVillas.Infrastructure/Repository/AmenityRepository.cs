fe47ab4c89f56dd48b3d7675570fb0145f74bcc0
Shri Nivas <mrshri404@gmail.com>
2025-03-12 21:16:23 +0530

    Add Amenity feature with CRUD operations
    This commit introduces a new `Amenity` feature to the application, enabling the creation, editing, deletion, and listing of amenities associated with villas. Key changes include:
    
    - Creation of the `IAmenityRepository` interface and its implementation in `AmenityRepository`.
    - Updates to the `IUnitOfWork` interface to manage amenities.
    - Definition of the `Amenity` entity class with necessary properties and validation.
    - Modifications to `VillaContext` to include a `DbSet<Amenity>` and seed data.
    - Creation of migration files for the new `Amenities` table.
    - Implementation of the `AmenityController` to handle HTTP requests.
    - Introduction of `AmenityVM` for data transfer between the controller and views.
    - Addition and modification of Razor views for managing amenities.
    - Update to the layout file to include navigation for the new feature.

---
+++ PrimeVillas.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,24 @@
+﻿using PrimeVillas.Application.Common.Interfaces;
+using PrimeVillas.Domain.Entities;
+using PrimeVillas.Infrastructure.DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeVillas.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+        private readonly VillaContext _context;
+        public AmenityRepository(VillaContext villaContext):base(villaContext)
+        {
+         _context = villaContext;   
+        }
+        public void Update(Amenity entity)
+        {
+          _context.Amenities.Update(entity);
+        }
+    }
+}
---
+++ PrimeVillasWeb/Controllers/AmenityController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PrimeVillas.Application.Common.Interfaces;
+using PrimeVillas.Domain.Entities;
+using PrimeVillasWeb.ViewModels;
+
+namespace PrimeVillasWeb.Controllers
+{
+    public class AmenityController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public AmenityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Amenity> amenities = _unitOfWork.Amenity.GetAll(includeProperties: "Villa");
+            return View(amenities);
+        }
+
+        public IActionResult Create()
+        {
+            AmenityVM amenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                })
+            };
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AmenityVM amenityVMObj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Amenity.Add(amenityVMObj.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "Amenity  added successfully";
+
+                return RedirectToAction("index");
+            }
+
+            amenityVMObj.VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(amenityVMObj);
+        }
+
+        public IActionResult Edit(int AmenityId)
+        {
+            AmenityVM  amenityVm = new()
+            {
+
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Amenity = _unitOfWork.Amenity.Get(u => u.Id == AmenityId)
+
+            };
+
+            if (amenityVm.Amenity == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(amenityVm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AmenityVM amenityVM)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _unitOfWork.Amenity.Update(amenityVM.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "Amenity Updated successfully";
+
+                return RedirectToAction("index");
+            }
+
+            amenityVM.VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            return View(amenityVM);
+        }
+
+
+        public IActionResult Delete(int AmenityId)
+        {
+            AmenityVM amenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Amenity = _unitOfWork.Amenity.Get(u => u.Id == AmenityId)
+            };
+
+            if (amenityVM.Amenity == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(amenityVM);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(AmenityVM amenityVM)
+        {
+            Amenity? obj = _unitOfWork.Amenity
+                .Get(u => u.Id == amenityVM.Amenity.Id);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            _unitOfWork.Amenity.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Amenity removed successfully.";
+            return RedirectToAction("index");
+        }
+    }
+}
